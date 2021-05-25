9627605dde184199614344f7e072d8d74f64790a
Dipak Rajendra Pawar <77726578+dipak-pawar131199@users.noreply.github.com>
2021-05-25 08:34:10 +0530

    Update MinimumLenString.py
    This program find Minimum length string from list of strings.

--- Assignment 4:(Strings)/MinimumLenString.py
+++ Assignment 4:(Strings)/MinimumLenString.py
@@ -1,1 +1,19 @@
 
+#8) Write a function that reads multiple strings, and will return the string in which the 
+#number of characters are minimum
+def minimumlenstr():
+   l=[]
+   lenl=[]
+   k=''
+   m=0
+   n=int(input("how many strings?"))
+   for i in range(n):
+       a=input("Enter string")
+       l.append(a)
+       lenl.append (len(a))
+   m=min(lenl)
+   for i in l:
+       if m==len(i):
+          k=i
+   print("\n",k,'is minimum length string you enter')
+minimumlenstr ()
